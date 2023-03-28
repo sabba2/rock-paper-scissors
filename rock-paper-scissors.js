63c30e7c214159b3bcf18c83390ebe2dcb3e390d
shayan-github <shayan_a_n@hotmail.com>
2023-03-28 14:07:02 +1000

    Create getComputerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+// Create a function that returns 'Rock', 'Paper', 'Scissors'
+
+// Generate random number between 0-2 and store it in num
+// if number is 0, Rock
+// if 1, Scissors
+// if 2, Paper
+// store in choice and return
+
+function getComputerChoice() {
+  let num = Math.floor(Math.random() * 3);
+  switch (num) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Scissors";
+    case 2:
+      return "Paper";
+  }
+}
